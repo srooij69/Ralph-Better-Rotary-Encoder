4b2623ec8b685df1070300921feff5c39f0543f3
Steven <srooij69@gmail.com>
2021-09-30 17:29:12 +0200

    Tweaked using the lookup directly

--- YetAnotherRotaryEncoder/README.md
+++ YetAnotherRotaryEncoder/README.md
@@ -1,3 +1,5 @@
 ### Here's the Arduino IDE sketch (works in PlatformIO too).
 
 #### Many thanks to Marko Pinteric (https://www.pinteric.com/rotary.html) for the original code.
+
+### And many thanks to Ralph Bacon for sharing his code. 
--- YetAnotherRotaryEncoder/YetAnotherRotaryEncoder.ino
+++ YetAnotherRotaryEncoder/YetAnotherRotaryEncoder.ino
@@ -1,100 +1,94 @@
 #include <Arduino.h>
 
+//Pins adjusted for use on a Arduino Nano/Uno
+
 // Rotary encoder pins
-#define PIN_A 32
+#define PIN_A 3 // was pin 32
 #define PIN_B 4
-#define PUSH_BTN 16
+#define PUSH_BTN 5 // was pin 16
 
 // A turn counter for the rotary encoder (negative = anti-clockwise)
 int rotationCounter = 200;
 
 // Flag from interrupt routine (moved=true)
-volatile bool rotaryEncoder = false;
+volatile bool hasRotaryEncoderChanged = false;
+volatile uint8_t rotary_state = 0;
 
-// Interrupt routine just sets a flag when rotation is detected
-void IRAM_ATTR rotary()
+#define ROTATE_CLOCKWISE  1
+#define NO_MOVEMENT   0
+#define ROTATE_ANTICLOCKWISE  -1
+
+int8_t rotation_direction[16];
+
+void init_rotation_direction(){
+    //start with no movement
+    for(uint8_t i=0; i<16; i++) {
+        rotation_direction[i]=NO_MOVEMENT;
+    }
+
+    rotation_direction[0b0011] = ROTATE_CLOCKWISE; //0011 => Right after previous Right
+   // rotation_direction[0b0101] = ROTATE_CLOCKWISE; //0101 => Right after previous Left
+    rotation_direction[0b0110] = ROTATE_ANTICLOCKWISE;  //0110 => Left after previous Left
+    rotation_direction[0b1001] = ROTATE_ANTICLOCKWISE;  //1001 => Left after previous Left
+    rotation_direction[0b1100] = ROTATE_CLOCKWISE; //1100 => Right after previous Right
+ //   rotation_direction[0b1110] = ROTATE_ANTICLOCKWISE;  //1110 => Left after previous Right
+}    
+
+// Interrupt routine just sets a flag when rotation_indication is detected
+void rotary_ISR()
 {
-    rotaryEncoder = true;
-}
+    hasRotaryEncoderChanged = true;
 
-// Rotary encoder has moved (interrupt tells us) but what happened?
-// See https://www.pinteric.com/rotary.html
-int8_t checkRotaryEncoder()
-{
-    // Reset the flag that brought us here (from ISR)
-    rotaryEncoder = false;
-
-    static uint8_t lrmem = 3;
-    static int lrsum = 0;
-    static int8_t TRANS[] = {0, -1, 1, 14, 1, 0, 14, -1, -1, 14, 0, 1, 14, 1, -1, 0};
-
-    // Read BOTH pin states to deterimine validity of rotation (ie not just switch bounce)
-    int8_t l = digitalRead(PIN_A);
-    int8_t r = digitalRead(PIN_B);
-
-    // Move previous value 2 bits to the left and add in our new values
-    lrmem = ((lrmem & 0x03) << 2) + 2 * l + r;
-
-    // Convert the bit pattern to a movement indicator (14 = impossible, ie switch bounce)
-    lrsum += TRANS[lrmem];
-
-    /* encoder not in the neutral (detent) state */
-    if (lrsum % 4 != 0)
-    {
-        return 0;
-    }
-
-    /* encoder in the neutral state - clockwise rotation*/
-    if (lrsum == 4)
-    {
-        lrsum = 0;
-        return 1;
-    }
-
-    /* encoder in the neutral state - anti-clockwise rotation*/
-    if (lrsum == -4)
-    {
-        lrsum = 0;
-        return -1;
-    }
-
-    // An impossible rotation has been detected - ignore the movement
-    lrsum = 0;
-    return 0;
+    // both D3 and D4 are on PORTD of a Nano
+    rotary_state = PIND;                       //Read the register for port D (pins A and B) directly
+    rotary_state = ( rotary_state & 0b0011000); //filter to allow values of D3 and D4 only
+    rotary_state = rotary_state>>3;             //make sure the 2 important values are on the LSB
 }
 
 void setup()
 {
     Serial.begin(115200);
+    init_rotation_direction();
 
     // The module already has pullup resistors on board
-    pinMode(PIN_A, INPUT);
-    pinMode(PIN_B, INPUT);
+    pinMode(PIN_A, INPUT); 
+    pinMode(PIN_B, INPUT); 
 
     // But not for the push switch
     pinMode(PUSH_BTN, INPUT_PULLUP);
 
     // We need to monitor both pins, rising and falling for all states
-    attachInterrupt(digitalPinToInterrupt(PIN_A), rotary, CHANGE);
-    attachInterrupt(digitalPinToInterrupt(PIN_B), rotary, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(PIN_A), rotary_ISR, CHANGE);
+ //   attachInterrupt(digitalPinToInterrupt(PIN_B), rotary_ISR, CHANGE);
     Serial.println("Setup completed");
 }
 
+uint8_t lookup_index = 0;
+
 void loop()
 {
-    // Has rotary encoder moved?
-    if (rotaryEncoder)
+    // Has rotary_ISR encoder moved?
+    if (hasRotaryEncoderChanged)
     {
-        // Get the movement (if valid)
-        int8_t rotationValue = checkRotaryEncoder();
-
+        hasRotaryEncoderChanged = false;
+        lookup_index = lookup_index<<2;             // Shift the old value over 2 bits
+        lookup_index = lookup_index | rotary_state; // Put the new value on the LSB
+        lookup_index = lookup_index & 0b00001111;   // Discard all but the 4 least significant bits
+        
+        int8_t rotation_increment = rotation_direction[lookup_index]; //Get the rotation_indication from the array   
+        
         // If valid movement, do something
-        if (rotationValue != 0)
+        char * rotation_indication=" - "; //rotation_indication is a tri state, L R or invalid
+        if (rotation_increment != 0)
         {
-            rotationCounter += rotationValue * 5;
-            Serial.print(rotationValue < 1 ? "L" :  "R");
-            Serial.println(rotationCounter);
+            rotationCounter += rotation_increment * 5;
+            rotation_indication = (rotation_increment < 1 ? " L " :  " R ");
+
         }
+        //having Serial.print ouside the if makes the glitches/bounces visible
+        Serial.print(lookup_index | 128, BIN);
+        Serial.print(rotation_indication);
+        Serial.println(rotationCounter);
     }
 
     if (digitalRead(PUSH_BTN) == LOW)
@@ -109,4 +103,5 @@ void loop()
             delay(100);
         }
     }
+    
 }
